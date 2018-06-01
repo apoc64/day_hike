b8f492de60ff6da616d40cdaf9da04863f530248
apoc64 <sschwedt@gmail.com>
2018-06-01 09:52:19 -0600

    visitor can view single trip

--- spec/features/visitor_visits_hiking_index_spec.rb
+++ spec/features/visitor_visits_hiking_index_spec.rb
@@ -15,4 +15,19 @@ describe 'visitor visits trips index' do
     expect(page).to have_content(trip1.name)
     expect(page).to have_content(trip2.name)
   end
+  # As a visitor,
+  # when I visit the hiking trips index page,
+  # and I click on the name of a hiking trip,
+  # I see a unique page for that hiking trip.
+  it 'can navigate to show page' do
+    trip1 = Trip.create(name: 'Grays')
+    trip2 = Trip.create(name: 'Torys')
+
+    visit trips_path
+
+    click_on trip1.name
+
+    expect(current_path).to eq(trip_path(trip1))
+    expect(page).to have_content(trip1.name)
+  end
 end
