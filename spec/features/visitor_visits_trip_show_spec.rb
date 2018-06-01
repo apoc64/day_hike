8ac96eeaa153d98b75ff10ec3f28e47569a19682
apoc64 <sschwedt@gmail.com>
2018-06-01 09:58:35 -0600

    tests trip show displays trails

---
+++ spec/features/visitor_visits_trip_show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'visitor visits hiking show' do
+# As a visitor,
+# when I visit a hiking trip's page,
+# I see a list of trails included in the trip,
+# I see the name, address, and length for each trail.
+  it 'shows all trials for that trip' do
+    trip = Trip.create(name: 'Grays and Torys')
+    trail1 = trip.trails.create(name: 'North trail', length: 5, address: '123 Mountain St')
+    trail2 = trip.trails.create(name: 'South trail', length: 7, address: '321 Mountain St')
+
+    visit trip_path(trip)
+
+    expect(page).to have_content(trail1.name)
+    expect(page).to have_content(trail1.length)
+    expect(page).to have_content(trail1.address)
+    expect(page).to have_content(trail2.name)
+    expect(page).to have_content(trail2.length)
+    expect(page).to have_content(trail2.address)
+  end
+end
